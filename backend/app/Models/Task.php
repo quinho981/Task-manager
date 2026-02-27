6898f4b5ee4cabe5f5ab9765cff307c1b05ddc29
Icaro Oliveira <i.oliveira@coblue.com.br>
2026-02-27 18:02:53 -0300

    - feat(project): add Project model and migration - feat(task): add Task  model and migration

---
+++ backend/app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'due_date' => 'datetime',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
---
+++ backend/database/migrations/2026_02_27_204629_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('todo');
+            $table->string('priority');
+            $table->datetime('due_date')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
