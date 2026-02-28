f339a6238c249bc22937d0b2342a00c6afa6537c
Icaro Oliveira <i.oliveira@coblue.com.br>
2026-02-28 17:04:48 -0300

    feat(api): add rate limiting

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,15 +4,17 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('projects')->group(function () {
-    Route::get('/', [ProjectController::class, 'index']);
-    Route::post('/', [ProjectController::class, 'store']);
-    
-    Route::get('/{projectId}/tasks', [TaskController::class, 'indexByProject']);
-    Route::post('/{projectId}/tasks', [TaskController::class, 'store']);
-});
+Route::middleware('throttle:60,1')->group(function () {
+    Route::prefix('projects')->group(function () {
+        Route::get('/', [ProjectController::class, 'index']);
+        Route::post('/', [ProjectController::class, 'store']);
+        
+        Route::get('/{projectId}/tasks', [TaskController::class, 'indexByProject']);
+        Route::post('/{projectId}/tasks', [TaskController::class, 'store']);
+    });
 
-Route::prefix('tasks')->group(function () {
-    Route::patch('/{id}', [TaskController::class, 'update']);
-    Route::delete('/{id}', [TaskController::class, 'destroy']);
+    Route::prefix('tasks')->group(function () {
+        Route::patch('/{id}', [TaskController::class, 'update']);
+        Route::delete('/{id}', [TaskController::class, 'destroy']);
+    });
 });
